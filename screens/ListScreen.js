efc989edc1439905e2c00eed5059401c50f428cf
Wanhee Kim <wanheekim@Wanheeui-MacBookPro.local>
2020-06-02 23:07:27 +0900

    first

---
+++ screens/ListScreen.js
@@ -0,0 +1,15 @@
+import { Ionicons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
+
+function ListScreen() {
+  return (
+    <View>
+      <Text>hello!</Text>
+    </View>
+  );
+}
+
+export default ListScreen;
